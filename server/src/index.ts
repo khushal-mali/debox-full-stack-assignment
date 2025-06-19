56df18d3aee678529392e16abffaa194e9fed0ea
Khushal Mali <khushalmali8448@gmail.com>
2025-06-19 20:24:38 +0530

    added routes

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,20 +1,33 @@
-import express, { Application, Request, Response, NextFunction } from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import express, { Application, NextFunction, Request, Response } from "express";
+import morgan from "morgan";
 import { connectMongoDB } from "./lib/db";
 import authRoutes from "./routes/auth.route";
+import categoryRoutes from "./routes/category.route";
+import csvRoutes from "./routes/csv.route";
+import inventoryRoutes from "./routes/inventory.route";
+import productRoutes from "./routes/product.route";
 
 dotenv.config();
 
 const app: Application = express();
-const PORT: number = parseInt(process.env.PORT || "5000", 10);
+const PORT: number = parseInt(process.env.PORT || "5000");
 
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Use morgan to log requests to the console
+app.use(morgan("dev"));
 
 // Routes
 app.use("/api/auth", authRoutes);
+app.use("/api/categories", categoryRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/inventory", inventoryRoutes);
+app.use("/api/upload", csvRoutes);
 
 // Error handling middleware
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
@@ -27,7 +40,7 @@ const startServer = async () => {
   try {
     await connectMongoDB();
     app.listen(PORT, () => {
-      console.log(`Server running on port ${PORT}`);
+      console.log(`Server running on --port ${PORT}`);
     });
   } catch (error) {
     console.error("Failed to start server:", error);
