09dc933ff2e7ae13d472ed6f0fa7f496c7424319
Khushal Mali <khushalmali8448@gmail.com>
2025-06-19 20:01:40 +0530

    roles type update

--- server/src/types/index.ts
+++ server/src/types/index.ts
@@ -1,5 +1,5 @@
 import { Request } from "express";
 
 export interface AuthRequest extends Request {
-  user?: { id: string; role: "Master" | "Admin" };
+  user?: { id: string; role: "MASTER" | "ADMIN" };
 }
